50d8c4d5e87964640ad09e839e5a5d0aa2913ba9
Matheus <matheusalexandre.sena99@gmail.com>
2021-05-15 19:37:47 -0300

    Add SVG

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { useColorScheme } from 'react-native';
+import { useColorScheme, Dimensions } from 'react-native';
 import Theme from './src/Theme';
 import { Container } from './src/components/Container';
 import { Text } from './src/components/Text';
+import Sun from './src/sources/sun.svg';
+import Moon from './src/sources/moon.svg';
+
+const { width } = Dimensions.get('window');
 
 const App = () => {
   const appearanceMode = useColorScheme();
@@ -10,6 +14,12 @@ const App = () => {
   return (
     <Theme>
       <Container>
+        {appearanceMode === 'light' ? (
+          <Sun width={width * 0.25} height={width * 0.25} fill={'#ff9800'} />
+        ) : (
+          <Moon width={width * 0.25} height={width * 0.25} fill={'#6200ee'} />
+        )}
+
         <Text>{appearanceMode}</Text>
       </Container>
     </Theme>
